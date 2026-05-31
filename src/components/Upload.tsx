af8b20c37de4e920e9b8907df6ee4e04f689ddbe
Igor Pereira <igrpereira.dev@gmail.com>
2026-05-31 16:53:37 -0300

    eventos form de envio de reembolso

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -2,18 +2,19 @@ import uploadSvg from "../assets/upload.svg"
 
 type Props = React.ComponentProps<"input"> & {
     filename?: string | null
+    isLoading?: boolean
 }
 
-export function Upload({ filename = null, ...rest }: Props) {
+export function Upload({ filename = null, isLoading, ...rest }: Props) {
     return (
         <div className="uppercase text-xs mb-2">
             <legend className="mb-2">Comprovante</legend>
             <div className="w-full h-12 pl-4 flex items-center rounded-lg border border-indigo-200">
-                <input type="file" id="upload" {...rest} className="hidden" />
+                <input type="file" id="upload" {...rest} className="hidden" disabled={isLoading} />
                 <span className="flex-1 text-[10px]">
                     {filename ?? "Clique para enviar o comprovante"}
                 </span>
-                <label htmlFor="upload" className="flex h-12 px-4 items-center rounded-r-lg bg-indigo-900 cursor-pointer -mr-px border disabled:opacity-50 hover:bg-indigo-700">
+                <label htmlFor="upload" className="flex h-12 px-4 items-center rounded-r-lg bg-indigo-900 cursor-pointer -mr-px border aria-disabled:opacity-50 hover:bg-indigo-700 transition ease-linear aria-disabled:cursor-progress aria-disabled:hover:bg-indigo-900" aria-disabled={isLoading}>
                     <img src={uploadSvg} alt="Ícone de upload" className="w-6 h-auto" />
                 </label>
             </div>
--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -1,19 +1,28 @@
+import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/category";
-import { useState } from "react";
+import { use, useState } from "react";
 
 export function Refund() {
+    const [name, setName] = useState("")
     const [category, setCategory] = useState("")
+    const [amount, setAmount] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [filename, setFilename] = useState<File | null>(null)
+
+    function onSubmit(e: React.SubmitEvent) {
+        e.preventDefault()
+    }
 
     return (
-        <form action="" className="w-full p-10 rounded-xl shadow-indigo-glow flex flex-col gap-6 lg:min-w-lg">
+        <form onSubmit={onSubmit} className="w-full p-10 rounded-xl shadow-indigo-glow flex flex-col gap-6 lg:min-w-lg">
             <header className="text-xl font-semibold">
                 <h1> Solicitação de Reembolso</h1>
                 <p className="text-xs font-extralight mt-2 mb-4">Por favor, preencha os dados abaixo para solicitar o reembolso.</p>
             </header>
-            <Input required legend="Nome da solicitação" />
+            <Input required legend="Nome da solicitação" value={name} onChange={(e) => setName(e.target.value)} />
             <div className="flex justify-between items-center gap-4">
                 <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
                     {
@@ -24,9 +33,10 @@ export function Refund() {
                         ))
                     }
                 </Select>
-                <Input legend="Valor" required />
+                <Input legend="Valor" required value={amount} onChange={(e) => setAmount(e.target.value)} />
             </div>
-            <Upload />
+            <Upload onChange={(e) => e.target.files && setFilename(e.target.files[0])} isLoading={isLoading} filename={filename && filename.name} />
+            <Button type="submit" isLoading={isLoading}>Enviar</Button>
         </form>
     )
 }
