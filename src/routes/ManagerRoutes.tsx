566a7d550c0d0004840a5664d6199c56f9e90282
Igor Pereira <igrpereira.dev@gmail.com>
2026-05-31 17:54:26 -0300

    rotas manager

---
+++ src/routes/ManagerRoutes.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router'
+import { Dashboard } from '../pages/Dashboard'
+import { NotFound } from '../pages/NotFound'
+import { AppLayout } from '../components/AppLayout'
+
+export function ManagerRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<AppLayout />}>
+                <Route path="/" element={<Dashboard />} />
+            </Route>
+
+            <Route path="*" element={<NotFound />} />
+        </Routes >
+    )
+}
