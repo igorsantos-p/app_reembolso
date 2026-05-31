0f67c8d4494f1e02ab9f66d8cd81431523e1164f
Igor Pereira <igrpereira.dev@gmail.com>
2026-05-31 18:24:45 -0300

    form busca de solicitacoes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,21 @@
+import { Input } from "../components/Input";
+import { useState } from "react";
+
 export function Dashboard() {
+    const [name, setName] = useState("");
+
+    function handleSearch(e: React.SubmitEvent) {
+        e.preventDefault()
+    }
+
     return (
-        <h1>Dashboard</h1>
+        <div className="rounded-lg p-10 shadow-indigo-glow md:min-w-3xl">
+            <h1 className="font-bold text-xl flex-1">Solicitações</h1>
+
+            <form onSubmit={handleSearch} className="flex flex-1 items-center justify-between pb-6 md:flex-row gap-2 mt-6
+            border-b border-indigo-900/60">
+                <Input placeholder="Buscar solicitações" onChange={(e) => setName(e.target.value)}></Input>
+            </form>
+        </div>
     )
 }
