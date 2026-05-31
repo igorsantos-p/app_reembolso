3efad415d72705c2124b8075dac9847ab230743e
Igor Pereira <igrpereira.dev@gmail.com>
2026-05-31 18:59:51 -0300

    ajuste button variantes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,21 @@
+import { classMerge } from "../utils/classMerge"
+
 type Props = React.ComponentProps<"button"> & {
     isLoading?: boolean
+    variant?: "base" | "icon"
 }
 
-export function Button({ children, isLoading, type = "button", ...rest }: Props) {
+const variants = {
+    button: {
+        base: "h-12 p-4",
+        icon: "h-12 w-12"
+    }
+}
+
+export function Button({ children, className, isLoading, type = "button", variant = "base", ...rest }: Props) {
     return (
-        <button type={type} {...rest} disabled={isLoading} className="uppercase p-4 rounded-lg flex items-center justify-center bg-indigo-900 cursor-pointer hover:bg-indigo-700 transition ease-linear disabled:opacity-50 disabled:cursor-progress disabled:hover:bg-indigo-900">{children}</button>
+        <button type={type} {...rest} disabled={isLoading} className={
+            classMerge(["uppercase rounded-lg flex items-center justify-center bg-indigo-900 cursor-pointer hover:bg-indigo-700 transition ease-linear disabled:opacity-50 disabled:cursor-progress disabled:hover:bg-indigo-900", variants.button[variant], className])
+        }>{children}</button>
     )
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,7 @@
+import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 import { useState } from "react";
+import searchSvg from "../assets/search.svg";
 
 export function Dashboard() {
     const [name, setName] = useState("");
@@ -15,6 +17,9 @@ export function Dashboard() {
             <form onSubmit={handleSearch} className="flex flex-1 items-center justify-between pb-6 md:flex-row gap-2 mt-6
             border-b border-indigo-900/60">
                 <Input placeholder="Buscar solicitações" onChange={(e) => setName(e.target.value)}></Input>
+                <Button variant="icon" type="submit">
+                    <img src={searchSvg} alt="Ícone de busca" className="w-6 h-auto" />
+                </Button>
             </form>
         </div>
     )
